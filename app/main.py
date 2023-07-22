6ac5b3c6df6593f814850cea4afce09e0f9e4ba5
LunarWnderer <76468151+LunarWnderer@users.noreply.github.com>
2023-07-22 18:38:51 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,12 +10,6 @@ async def get_smiley():
     random_smiley = random.choice(smileys)
     return {"smiley": random_smiley}
 
-# Main program loop
-for i in range(10, 1000, speed):
-    draw_smiley(i % 4)
-    time.sleep(speed)
-
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
